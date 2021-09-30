34d4b8b214ff7a2b131f4915a08dffb300df1096
Vadim Zubko <yjiotpukc@gmail.com>
2021-09-30 20:41:02 +0300

    Modifies config for GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     linkActiveClass: '',
     routes
 });
