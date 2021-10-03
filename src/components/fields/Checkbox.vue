b4544c4fd5958578752caa8841332fad4bd1a639
Vadim Zubko <yjiotpukc@gmail.com>
2021-10-03 14:52:40 +0300

    Adds Checkbox component

---
+++ src/components/fields/Checkbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pure-control-group">
+    <label :for="id">{{ name }}</label>
+    <input type="checkbox" :id="id" :checked="modelValue" @input="$emit('update:modelValue', $event.target.checked)"/>
+    <span v-if="description" class="pure-form-message-inline">{{ description }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string,
+  modelValue: string,
+  id: string,
+  description?: string,
+}>();
+
+defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>();
+</script>
